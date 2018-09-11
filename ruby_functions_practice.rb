45aaf1a5f8161a57db33d1d86ebefece6a839039
Michael Griffin <michaelgriffin121@gmail.com>
2018-09-11 15:25:25 +0100

    .

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,8 @@ end
 def join_string(string_1,string_2)
 return "#{string_1}" + "#{string_2}"
 end
+
+def add_string_as_number(string1, string2)
+add_result = string1.to_i + string2.to_i
+return add_result
+end

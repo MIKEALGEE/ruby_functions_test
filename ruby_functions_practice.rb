838991eb43d533282e745706c2b4098cd09d95c2
Michael Griffin <michaelgriffin121@gmail.com>
2018-09-11 15:49:38 +0100

    .

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,3 +30,34 @@ def add_string_as_number(string1, string2)
 add_result = string1.to_i + string2.to_i
 return add_result
 end
+
+def number_to_full_month_name(number)
+case number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+      when 6
+          return "June"
+        when 7
+            return "July"
+          when 8
+              return "August"
+            when 9
+                return "September"
+              when 10
+                  return "Octubre"
+                when 11
+                    return "November"
+                  when 12
+                      return "Dicember"
+                  else
+                    return "Not a Month"
+end
+end
